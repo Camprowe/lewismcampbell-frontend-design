a31e1cddbe3f485cac0ed22b90a2ee21fd063782
Lewis Campbell <5160938+lewismcampbell@users.noreply.github.com>
2022-11-10 21:41:26 +0000

    Colours Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#FF4500',
+      'secondary': '#00BAFF',
+      'dark': '#1B2430',
+      'light': '#F9F9F9'
+    },
     extend: {
       fontFamily: {
         'noto-sans': ['"Noto Sans"', 'sans-serif'],
